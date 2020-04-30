e94cf782a13e25d9049dfa13b8705545565554a6
jonasmamona <jonasmpdc@gmail.com>
2020-04-30 10:30:00 -0300

    modified datediff to use math.floor instead of math.round

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,14 +1,6 @@
 export function datediff(date1: Date, date2: Date) {
   const millisecondsInADay = 86400000;
-  let result = Math.round(
-    (date2.valueOf() - date1.valueOf()) / millisecondsInADay
-  );
-
-  if (isNaN(result)) {
-    return 0;
-  }
-
-  return result;
+  return Math.floor((date2.valueOf() - date1.valueOf()) / millisecondsInADay);
 }
 
 export function changeTimezone(date: Date, newTimezone: string) {
